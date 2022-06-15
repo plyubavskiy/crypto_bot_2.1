a5de2676ce74384904ed10d02cdd1f1de3706b63
plyubavskiy <106429616+plyubavskiy@users.noreply.github.com>
2022-06-15 13:51:07 +0300

    Update beta_2.1.py

--- beta_2.1.py
+++ beta_2.1.py
@@ -1,27 +1,16 @@
 from binance.client import Client
 from binance.websockets import BinanceSocketManager
-
 import time
-
 import math
 
-
 api_key = 'SPECIAL_KEY'
-
 api_secret = 'SECRETKA'
-
 client = Client(api_key, api_secret)
 
-
 ASSET = 'ETHUSDT'
-
 CURRENCY = 'USDT'
-
 CRYPTOCURRENCY = 'ETH'
-
 START_CRYPTOCURRENCY = 'my_balance'
-
-
 
 
 def balance(symbol):
@@ -30,13 +19,9 @@ def balance(symbol):
     return balance
 
 
-
-
 def history(symbol):
     history = client.get_my_trades(symbol=symbol)
     return history
-
-
 
 
 def price(symbol):
@@ -44,38 +29,24 @@ def price(symbol):
     return float(price)
 
 
-
-
 def order_market_buy(quantity):
     order = client.order_market_buy(symbol=ASSET, quantity=quantity)
-
-
-
 
 
 def order_market_sell(quantity):
     order = client.order_market_sell(symbol=ASSET, quantity=quantity)
 
-
-
 import telebot
-
-
 bot = telebot.TeleBot('Токен')
-
 ID = 'мой айдишник в папке - Телеграм ID'
-
 
 
 def message(text):
     bot.send_message(ID, text)
 
 
-
 def buy_message_success():
-
     data = history(ASSET)[-1]
-
     message('Покупка')
     message('Информация о сделке \n\nРынок: ' + data['symbol'] + '\nПокупка: ' + data[
         'commissionAsset'] + '\nКупленный актив: ' + data['qty'] + ' ' + data[
@@ -89,11 +60,8 @@ def buy_message_success():
         float(balance(CRYPTOCURRENCY)['free']) - START_CRYPTOCURRENCY))
 
 
-
 def sell_message_success():
-
     data = history(ASSET)[-1]
-
     message('Продажа')
     message('Информация о сделке \n\nРынок: ' + data['symbol'] + '\nПокупка: ' + data[
         'commissionAsset'] + '\nКупленный актив: ' + data['quoteQty'] + ' ' + data[
@@ -106,65 +74,40 @@ def sell_message_success():
     message('Прибыль\n\nПрибыль ' + CRYPTOCURRENCY + ': ' + str(
         float(balance(CRYPTOCURRENCY)['free']) - START_CRYPTOCURRENCY))
 
-
-
 TIME = 10
-
 GROW_PERCENT = 0.5
-
 FALL_PERCENT = -0.25
 
 
-
 def main(FIRST_PRICE):
-
     def toFixed(f: float, n=0):
         a, b = str(f).split('.')
         return '{}.{}{}'.format(a, b[:n], '0' * (n - len(b)))
-
-
     time.sleep(TIME)
-
     PRICE = price(ASSET)
-
     PROCENT = ((PRICE - FIRST_PRICE) / FIRST_PRICE) * 100
-
     print('Цена отсчета:', str(FIRST_PRICE), '| Процент:', str(PROCENT))
-
-
     if PROCENT >= GROW_PERCENT:
         try:
             print('BUY')
-
             order_market_buy(toFixed(float(balance(CURRENCY)['free']) / price(ASSET), 5))
-
             buy_message_success()
-
             main(PRICE)
         except:
             print('Ошибка при покупке!')
-
             main(PRICE)
-
     elif PROCENT <= FALL_PERCENT:
         try:
             print('SELL')
-
             order_market_sell(toFixed(float(balance(CRYPTOCURRENCY)['free']), 5))
-
             sell_message_success()
-
             main(PRICE)
         except:
             print('Ошибка при продаже!')
-
             main(PRICE)
-
-    else:
-        main(FIRST_PRICE)
-
+        else:
+            main(FIRST_PRICE)
 
 
 START_PRICE = price(ASSET)
-
 main(START_PRICE)
